39921354ad4509a4430aaea3f1465c80cab85b3c
sipranto97 <49632674+sipranto97@users.noreply.github.com>
2021-02-28 22:52:14 +0600

    Update settings.py

--- assignment2/settings.py
+++ assignment2/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '@&hbdfma)b^e+-w1pbcfz_v%q$2*xtdoj@oh2@ag4$lpc28$7a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','foodmenubd.herokuapp.com']
 
 
 # Application definition
