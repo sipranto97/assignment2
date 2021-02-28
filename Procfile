3126ad318e58d89cca12846957b39437ee8f9da2
sipranto97 <49632674+sipranto97@users.noreply.github.com>
2021-02-28 22:25:31 +0600

    FoodApp

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:gunicorn assignment2.wsgi
---
+++ food/urls.py
@@ -0,0 +1,16 @@
+from . import views
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+app_name = 'food'
+urlpatterns = [
+    path('',views.index, name='index'),
+    path('<int:item_id>/',views.detail,name='detail'),
+    path('item/',views.item,name='item'),
+    path('add',views.create_item,name='create_item'),
+    path('update/<int:id>/',views.update_item, name='update_item'),
+    path('delete/<int:id>/',views.delete_item, name='delete_item'),
+
+]
